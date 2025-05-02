76f92ad1c0cd4df3240fda40439123217ef7dfc4
francesco7773434 <francescoorbello5@gmail.com>
2025-05-02 11:00:06 +0200

    Initial commit

---
+++ src/main/java/it/epicode/games_retro/GamesRetroApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.games_retro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesRetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesRetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/games_retro/GamesRetroApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.games_retro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesRetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
